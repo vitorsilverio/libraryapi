d2c7d1bdd5fcbf044cbe85e22ca96edaddf4be91
Jaider Andrade Ferreira <jaideraf@gmail.com>
2021-09-24 14:28:47 -0300

    pylintrc and suggestions

--- libraryapi/pergamum.py
+++ libraryapi/pergamum.py
@@ -1,19 +1,22 @@
 
+import subprocess
+from io import BytesIO
 from requests import Session
 from zeep import Client
 from zeep.transports import Transport
-from io import BytesIO
-import subprocess
+
 
 class Pergamum:
 
     def __init__(self, base_url: str) -> None:
         session = Session()
         session.headers.update({'Accept-Encoding': 'identity'})
-        self.client = Client(f'{base_url}/web_service/servidor_ws.php?wsdl', transport=Transport(session=session))
+        self.client = Client(
+            f'{base_url}/web_service/servidor_ws.php?wsdl', transport=Transport(session=session))
 
     def busca_marc(self, cod_acervo: int) -> str:
         return self.client.service.busca_marc(codigo_acervo_temp=cod_acervo)
+
 
 class PergamumDownloader:
 
@@ -27,10 +30,9 @@ class PergamumDownloader:
     def download_iso(self, url: str, id: int):
         self._add_base(url)
         marc_xml = self.base[url].busca_marc(id)
-        process = subprocess.run(['saxonb-xslt', '-xsl:pergamumSoap-MarcXML.xsl', '-s:/dev/stdin' ], input=bytes(marc_xml, encoding='utf-8'), capture_output=True)
-        process = subprocess.run(['yaz-marcdump', '-i', 'marcxml', '-o', 'marc', '-l', '5=110,9=97,18=97', '/dev/stdin'], input=process.stdout, capture_output = True)
-           
+        process = subprocess.run(['saxonb-xslt', '-xsl:pergamumSoap-MarcXML.xsl', '-s:/dev/stdin'],
+                                 input=bytes(marc_xml, encoding='utf-8'), capture_output=True, check=True)
+        process = subprocess.run(['yaz-marcdump', '-i', 'marcxml', '-o', 'marc', '-l',
+                                 '5=110,9=97,18=97', '/dev/stdin'], input=process.stdout, capture_output=True, check=True)
 
         return BytesIO(process.stdout)
-
-    
